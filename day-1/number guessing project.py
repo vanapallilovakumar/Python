a0c568da10e4836868121fb812f0a15bf3b789f1
lovakumar <vanapallilovakumar07@gmail.com>
2026-07-20 23:40:07 +0530

    Updated number guessing project

--- day-1/number guessing project.py
+++ day-1/number guessing project.py
@@ -1,6 +1,9 @@
 a=(5,6,7,8,9)
-a=("user guess 5:9")
-print("user_guess specific number:7" )
-print=int(input(a))
-
+secret_number=7
+int(input("enter your guess:"))
+#if and else statements helps to check true or false
+if guess ==secret_number:
+  print("it is correct!!!")
+else:
+  print("it is not correct!!")
 
